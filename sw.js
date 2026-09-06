5ee75cb1e1361e815733864725b622385dee8284
kesga44-jpg <kesga44@gmail.com>
2026-09-06 18:51:19 +0200

    Update v11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='samen-thuis-v10-travel-import';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='samen-thuis-v11-travel-priority-sync';
+const ASSETS=['./','./index.html','./styles.css','./upgrade-v11.css','./app.js','./upgrade-v11.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
