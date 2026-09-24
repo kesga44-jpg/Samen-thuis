e106871c8f57d1b718c352a34be413912627a8fa
kesga44-jpg <kesga44@gmail.com>
2026-09-24 23:06:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='samen-thuis-v24-1-rdw';
-const ASSETS=['./','./index.html','./styles.css?v=241','./app.js?v=241','./manifest.webmanifest','./icon.svg'];
+const CACHE='samen-thuis-v24-2-fuelmate-stats';
+const ASSETS=['./','./index.html','./styles.css?v=242','./app.js?v=242','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
